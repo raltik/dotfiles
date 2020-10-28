71060519d47c8a99b89b44c529395296b8f128b8
Raul Ferrando <rauferpl@gmail.com>
2020-10-28 14:46:18 +0100

    Update script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt update
 
 echo -e "\n Installing packages...\n"
-sudo apt install zsh neovim git curl
+sudo apt install zsh neovim git curl wget gitg
 
 echo -e "\n Installing Oh My Zsh"
 
@@ -32,4 +32,24 @@ cp $DOT/zshrc $HOME/.zshrc
 
 chsh -s $(which zsh)
 
+echo -e "\nInstalling spotify."
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install spotify-client spotify-client-gnome-support
+
+echo -e "\nInstalling VSCode"
+cd /tmp
+wget https://az764295.vo.msecnd.net/stable/d2e414d9e4239a252d1ab117bd7067f125afd80a/code_1.50.1-1602600906_amd64.deb -O code.deb
+sudo apt install ./code.deb
+
+echo -e "\nInstalling Telegram"
+sudo apt install telegram-desktop
+
+echo -e "\nInstalling Discord"
+wget https://dl.discordapp.net/apps/linux/0.0.12/discord-0.0.12.deb -O discord.deb
+sudo apt install ./discord.deb
+
+
 echo "Restart the computer."
+
