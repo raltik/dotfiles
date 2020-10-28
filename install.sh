ea192b818ee0dfa65a8e83162a5966530ab20865
Raul Ferrando <rauferpl@gmail.com>
2020-10-28 14:39:58 +0100

    Update script

--- install.sh
+++ install.sh
@@ -2,13 +2,19 @@
 
 sudo apt update
 
+echo -e "\n Installing packages...\n"
 sudo apt install zsh neovim git curl
+
+echo -e "\n Installing Oh My Zsh"
 
 #Oh my Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo -e "\n Installing theme"
 #Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo -e "\n Creating directories."
 
 mkdir -p $HOME/.sources/bin
 mkdir -p $HOME/.config/nvim
