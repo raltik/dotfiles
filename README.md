c88ca148210a2ff609985a9e7e7d39307fe3aae0
Raul Ferrando <rauferpl@gmail.com>
2021-03-19 12:13:48 +0100

    VIM. ZSH. Gruvbox

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # Dotfiles
 
-Execute script 'install.sh'
-
-### Prerequisites
-* Zsh
-* [Oh-My-Zsh](https://ohmyz.sh/)
-* [NeoVim](https://neovim.io/)
-* [Powerlevel10k](https://github.com/romkatv/powerlevel10k)
-
-### NeoVim
-Put the file [init.vim](https://github.com/raltik/dotfiles/blob/master/neovim/init.vim) in _'~/.config/nvim/init.vim'_ 
--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt update
 
 echo -e "\n Installing packages...\n"
-sudo apt install zsh neovim git curl wget gitg alacritty tmux
+sudo apt install zsh vim curl wget tmux
 
 echo -e "\n Installing Oh My Zsh"
 
@@ -17,29 +17,20 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 echo -e "\n Creating directories."
 
 mkdir -p $HOME/.sources/bin
-mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.local/share/fonts
-mkdir -p $HOME/.config/alacritty
 mkdir -p $HOME/.config/tmuxinator
-
-cd $HOME/
 
 DOT=$HOME/code/dotfiles
 
-cp $DOT/zsh $HOME/.sources
-cp $DOT/neovim/init.vim $HOME/.config/nvim
+cp $DOT/vimrc $HOME/.vimrc
 cp $DOT/fonts/* $HOME/.local/share/fonts
 cp $DOT/p10k.zsh $HOME/.p10k.zsh
 cp $DOT/zshrc $HOME/.zshrc
 
-cp $DOT/alacritty/alacritty.yml $HOME/.config/alacritty
 cp $DOT/tmux/tmux.conf $HOME/.tmux.conf
 cp $DOT/tmux/tmuxinator/tfg.yml $HOME/.config/tmuxinator/tfg.yml
 
 chsh -s $(which zsh)
-
-echo -e "\nInstalling Telegram"
-sudo apt install telegram-desktop
 
 echo "Restart the computer."
 
