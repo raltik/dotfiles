b9a57c39665609e9571a384be74f7b8835c0a2d6
Raul Ferrando <>
2021-05-09 19:04:20 +0200

    Script modified

--- install.sh
+++ install.sh
@@ -3,32 +3,26 @@
 sudo apt update
 
 echo -e "\n Installing packages...\n"
-sudo apt install zsh vim curl wget tmux tmuxinator
+sudo apt install zsh neovim curl wget tmux tmuxinator
 
 echo -e "\n Installing Oh My Zsh"
 
 #Oh my Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo -e "\n Installing theme"
+echo -e "\n Installing POWERLEVEL10K"
 #Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo -e "\n Creating directories."
 
 mkdir -p $HOME/.sources/bin
-mkdir -p $HOME/.local/share/fonts
 mkdir -p $HOME/.config/tmuxinator
 
 DOT=$HOME/code/dotfiles
 
 cp $DOT/vimrc $HOME/.vimrc
-cp $DOT/fonts/* $HOME/.local/share/fonts
-cp $DOT/p10k.zsh $HOME/.p10k.zsh
 cp $DOT/zshrc $HOME/.zshrc
-
-cp $DOT/tmux/tmux.conf $HOME/.tmux.conf
-cp $DOT/tmux/tmuxinator/tfg.yml $HOME/.config/tmuxinator/tfg.yml
 
 chsh -s $(which zsh)
 
